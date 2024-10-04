afe5045b5775514b7b1d72e3e62f0f927d58b20f
kabasunny <dxbqd553@yahoo.co.jp>
2024-10-04 20:16:57 +0900

    ちょい修正

--- frontend/src/components/Admin/AddQuestion.tsx
+++ frontend/src/components/Admin/AddQuestion.tsx
@@ -1,5 +1,4 @@
-import React, { useState, useRef, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useRef } from 'react';
 import axios from 'axios';
 import './css/AdminScreen.css';
 import './css/AddQuestion.css';
